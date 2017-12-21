0bad9eb1f3c6443268a751d2e0cb5f6fa0ae9a0a
lockonexos <34747678+lockonexos@users.noreply.github.com>
2017-12-21 17:50:05 +0000

    Added reroll

--- dice-rolling.py
+++ dice-rolling.py
@@ -1,5 +1,9 @@
 import random
-min = 1
-max = 6
-answer = random.randint(min,max)
-print(answer)
+
+option = "Y"
+while not option.lower() == "n":
+	min = 1
+	max = 6
+	answer = random.randint(min,max)
+	print(answer)
+	option = input("Would you like to roll again? (Y/N) :")
